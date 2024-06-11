0709274c09d355a8f1b519704078a9f267d54e81
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-06-11 17:05:11 +0300

    Fixed typos, added comments and modified the functions.

--- LAB_LISTS_TUPLES.py
+++ LAB_LISTS_TUPLES.py
@@ -7,14 +7,19 @@ def sum_int(items:list) -> int:
 
     Returns:
         result (int): The sum of all integers in the list,
-                      Returns 0 if no integers were found.
+                      Returns None if there is a non-integer element.
     '''
-
-    result = 0
+    # Check that all elements in the list are integers.
     for item in items:
-        # Check if the current item is an integer
+        # Continue looping if the element is integer.
         if type(item) == int:
-            result += item
+            continue
+        # Terminate the function when encountering a non-integer element.
+        else:
+            print("The list must have integer elements only.")
+            return None
+    result = 0
+    result += item
     return result
 
 def greatest_int(items:list) -> int:
@@ -26,18 +31,40 @@ def greatest_int(items:list) -> int:
 
     Returns:
         greatest (int): The greatest integer in the list.
-                        Returns 0 if no integers were found.
+                        Returns None if there is a non-integer element.
     '''
-    greatest = 0
+        # Check that all elements in the list are integers.
     for item in items:
+        # Continue looping if the element is integer.
         if type(item) == int:
-            if item > greatest:
-                greatest = item
+            continue
+        # Terminate the function when encountering a non-integer element.
+        else:
+            print("The list must have integer elements only.")
+            return None
+    greatest = items[0]
+    for item in items:
+         if item > greatest:
+            greatest = item
     return greatest
 
-print(sum_int([2, 3, 4, 5, 15, 1, 43, 20]))
-print(greatest_int([2, 3, 4, 5, 15, 1, 43, 20]))
-odd_numbers:list = [x for x in range(1200, 2000, 125)]
+## Given the following list : [2, 3, 4, 5, 15, 1, 43, 20]
+the_list:list = [2, 3, 4, 5, 15, 1, 43, 20]
+
+### Q1: Write a function that takes a list as a parameter,
+### and then returns  the sum  of all the items in that list.
+print(sum_int(the_list))
+
+### Q2: Write a function that takes a list as a parameter,
+### and then returns the largest number from a list of numbers.
+print(greatest_int(the_list))
+
+### Q3: Create an odd numbers list from the elements of a range
+### from 1200 to 2000 with steps of 125, using [ List Comprehension ].
+odd_numbers:list = [x for x in range(1200, 2000, 125) if x % 2 != 0]
 print(odd_numbers)
-sliced_odd_numbers:list = odd_numbers[:6]
+
+### Q4: use list slicing to get a new list from the previous list
+### (odd numbers list) starting from the start to the 5th element in the list.
+sliced_odd_numbers:list = odd_numbers[:5]
 print(sliced_odd_numbers)
