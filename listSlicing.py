ff8d20807a5ecf201388633ca6bbf0db5b5140b7
D. F <ifonei@hotmail.com>
2024-06-11 15:56:57 +0300

    Slicinge the list job finished

---
+++ listSlicing.py
@@ -0,0 +1,13 @@
+#Getting a new list from the list [oddNumbers], 
+#starting from the start to the 5th element in the list.
+oddNumbers = [n for n in range(1200,2000,125) if n % 2 != 0]
+print(oddNumbers)
+new_list = oddNumbers[:6]
+print(new_list)
+
+
+
+
+
+
+
