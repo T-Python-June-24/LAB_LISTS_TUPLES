295432af301185109e7119245e6803a183b44e9e
9twy <saeed.abdullah.alghamdi@outlook.sa>
2024-06-12 10:09:39 +0300

    Solved by saeed

--- LAB_LISTS_TUPLES.py
+++ LAB_LISTS_TUPLES.py
@@ -22,7 +22,6 @@ def maxmum(numbers:int)->int:
     return max(randomNumbers) #Output:43
 
 # creating odd numbers in list
-oddNumbers=[x for x in range(1200,2000+1,125) if  x%2!=0] #Output:[1200, 1325, 1450, 1575, 1700, 1825, 1950]
-print(oddNumbers)
+oddNumbers=[x for x in range(1200,2000+1,125) if  x%2!=0] #Output:[1325, 1575, 1825]
 #slicing odd numbers
 slicedOddNumbers=oddNumbers[:5]
